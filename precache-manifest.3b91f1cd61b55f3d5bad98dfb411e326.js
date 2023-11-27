fca6298c5c050b53ab532c50f99a408a04edb057
Naresh Namani <namaninaresh1996@gmail.com>
2023-11-27 19:08:19 +0530

    Updates

---
+++ precache-manifest.3b91f1cd61b55f3d5bad98dfb411e326.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae1124dad062a3cfb2dea64715e5ba3",
+    "url": "/CovidAppReact/index.html"
+  },
+  {
+    "revision": "36d83382ee82e088014b",
+    "url": "/CovidAppReact/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a6256d78fd9e7882409f",
+    "url": "/CovidAppReact/static/css/5.7dff9af4.chunk.css"
+  },
+  {
+    "revision": "18ab547729589dadf812",
+    "url": "/CovidAppReact/static/css/main.1ca3701d.chunk.css"
+  },
+  {
+    "revision": "36d83382ee82e088014b",
+    "url": "/CovidAppReact/static/js/2.7b45637d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CovidAppReact/static/js/2.7b45637d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebfdd99f9c1642092ce2",
+    "url": "/CovidAppReact/static/js/3.22e2ef0a.chunk.js"
+  },
+  {
+    "revision": "aaf1917767379db72497",
+    "url": "/CovidAppReact/static/js/4.be58fd0d.chunk.js"
+  },
+  {
+    "revision": "a6256d78fd9e7882409f",
+    "url": "/CovidAppReact/static/js/5.8bf1f786.chunk.js"
+  },
+  {
+    "revision": "62e4307bdc33020a7d5d",
+    "url": "/CovidAppReact/static/js/6.afcac563.chunk.js"
+  },
+  {
+    "revision": "18ab547729589dadf812",
+    "url": "/CovidAppReact/static/js/main.7aaf8740.chunk.js"
+  },
+  {
+    "revision": "39582187ec427051d92c",
+    "url": "/CovidAppReact/static/js/runtime-main.8110b8ff.js"
+  }
+]);
